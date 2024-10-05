4d8c7a8f03e47feec6e33f55debb032a7b3502e8
margarita2104 <margarita.p.ferreira@gmail.com>
2024-10-05 17:16:50 +0200

    removed unused import

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,3 @@
-import Header from "@/app/components/header/Header";
-
 export default function Home() {
   // This part is used for the TypeScript functions
   // If you're not familiar with TS, you can add a JavaScript file instead
