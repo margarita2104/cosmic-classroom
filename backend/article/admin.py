4eb9f911f1776cad0d0b72247d6edd9765f81cbb
margarita2104 <margarita.p.ferreira@gmail.com>
2024-10-05 12:56:02 +0200

    article model

--- backend/article/admin.py
+++ backend/article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'description', 'link', 'image',)
+
+  admin.site.register(Article, ArticleAdmin)
