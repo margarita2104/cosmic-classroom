dffe0673b938b28747d81eebfcc0d4d15883fd8e
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-10-05 12:02:25 +0200

    create user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Field used for authentication
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(blank=True, null=True, unique=False, max_length=100)
+    country = CountryField()
+    favorite_resources = models.ManyToManyField("Article")
+
+
+    def __str__(self):
+        return self.username
