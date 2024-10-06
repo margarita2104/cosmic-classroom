f2bfe1799efcafd55b814469a846e86899db2925
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-10-06 16:58:19 +0200

    create user prompt field in user model

--- backend/lesson/views.py
+++ backend/lesson/views.py
@@ -18,21 +18,26 @@ class LessonCreateListView(ListCreateAPIView):
         serializer = self.get_serializer(data = lesson_data)
         serializer.is_valid(raise_exception=True)
         
+        user_prompt = serializer.validated_data.get('user_prompt')
         subject = serializer.validated_data.get('subject')
         grade_level = serializer.validated_data.get('grade_level')
         class_size = serializer.validated_data.get('class_size')
         technology_access = serializer.validated_data.get('technology_access')
         class_length = serializer.validated_data.get('class_length')
         learning_styles = serializer.validated_data.get('learning_styles')
-        user_input = f"""
-        I am a {grade_level} teacher.
-        I have {class_size} students in my classroom.
-        We are interested in {subject}.
-        Our preferred learning style is {learning_styles}.
-        The class lasts {class_length} minutes.
-        We have {technology_access} technology access.
-        """
-        pdf_file_path, lesson_plan = create_lesson_plan_from_user_input(user_input)
+        
+        if user_prompt:
+            pdf_file_path, lesson_plan = create_lesson_plan_from_user_input(user_prompt)
+        else:    
+            user_input = f"""
+            I am a {grade_level} teacher.
+            I have {class_size} students in my classroom.
+            We are interested in {subject}.
+            Our preferred learning style is {learning_styles}.
+            The class lasts {class_length} minutes.
+            We have {technology_access} technology access.
+            """
+            pdf_file_path, lesson_plan = create_lesson_plan_from_user_input(user_input)
         serializer.save(created_lesson=lesson_plan)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
