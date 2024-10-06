b75d6f63c7a33d0ff17d7f314edc4bddf0119151
margarita2104 <margarita.p.ferreira@gmail.com>
2024-10-06 11:35:44 +0200

    changed the base url

--- frontend/app/axios/Axios.tsx
+++ frontend/app/axios/Axios.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:8000/"
-    : "https://cosmic-classroom.vercel.app";
+    : "https://cosmicclassroom.co/";
 
 export const AxiosCosmicClassroom = axios.create({
   baseURL: BASE_URL,
